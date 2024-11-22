d176b4b1836aa2b2e8b3ed57fa7bf23ab62073fd
Dom <86100131+dj4815@users.noreply.github.com>
2024-11-22 20:36:53 +0100

    m

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 const button = document.querySelector("button");
-const body = document.querySelector("body");
-let currentColorValues = document.querySelector("p");
-let currentBackgroundColor = body.style.backgroundColor;
+// const body = document.querySelector("body");
+let paragraph = document.querySelector("p");
 
 button.addEventListener("click", () => {
-  let r = `${Math.floor(Math.random() * 255)}, `;
+  const r = `${Math.floor(Math.random() * 255)}, `;
   // console.log(r);
-  let g = `${Math.floor(Math.random() * 255)}, `;
+  const g = `${Math.floor(Math.random() * 255)}, `;
   // console.log(g);
-  let b = `${Math.floor(Math.random() * 255)}`;
+  const b = `${Math.floor(Math.random() * 255)}`;
   // console.log(b);
   // console.log(currentBackgroundColor);
-
+  const newColor = `rgb(${r}${g}${b})`;
   // Set current background color to the freshly generated values
-  currentBackgroundColor = `rgb(${r}${g}${b})`;
+  document.body.style.backgroundColor = newColor;
 
   // Set the text of the paragraph to the new color values
-  currentColorValues.innerText = `Current color: ${currentBackgroundColor}`;
+  paragraph.innerText = `Current color: ${currentBackgroundColor}`;
 
   // Replace the current background color with the new background color
   body.style.backgroundColor = `${currentBackgroundColor}`;
