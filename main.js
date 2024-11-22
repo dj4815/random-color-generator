75a17bd7c50db06a2fc2ffed5417f581c6e31ba5
Dom <86100131+dj4815@users.noreply.github.com>
2024-11-22 15:03:38 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+import "./style.css";
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+let currentColorValues = document.querySelector("p");
+let currentBackgroundColor = body.style.backgroundColor;
+
+button.addEventListener("click", () => {
+  let r = `${Math.floor(Math.random() * 255)}, `;
+  // console.log(r);
+  let g = `${Math.floor(Math.random() * 255)}, `;
+  // console.log(g);
+  let b = `${Math.floor(Math.random() * 255)}`;
+  // console.log(b);
+  // console.log(currentBackgroundColor);
+
+  // Set current background color to the freshly generated values
+  currentBackgroundColor = `rgb(${r}${g}${b})`;
+
+  // Set the text of the paragraph to the new color values
+  currentColorValues.innerText = `Current color: ${currentBackgroundColor}`;
+
+  // Replace the current background color with the new background color
+  body.style.backgroundColor = `${currentBackgroundColor}`;
+
+  // console.log(rgb);
+});
