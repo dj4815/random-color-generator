1e394ba2e8cffbcc8233084c934ef7bbc4c0d44c
Dom <86100131+dj4815@users.noreply.github.com>
2024-11-22 21:28:35 +0100

    m

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 const button = document.querySelector("button");
 // const body = document.querySelector("body");
-let h2 = document.querySelector("h2");
+const h2 = document.querySelector("h2");
 
 button.addEventListener("click", () => {
-  const r = `${Math.floor(Math.random() * 255)}, `;
-  // console.log(r);
-  const g = `${Math.floor(Math.random() * 255)}, `;
-  // console.log(g);
-  const b = `${Math.floor(Math.random() * 255)}`;
-  // console.log(b);
-  // console.log(currentBackgroundColor);
-  const newColor = `rgb(${r}${g}${b})`;
+  const newColor = makeRandomColor();
+
   // Set current background color to the freshly generated values
   document.body.style.backgroundColor = newColor;
 
-  // Set the text of the paragraph to the new color values
+  // Set the text of the h2 to the new color values
   h2.innerText = `Current color: ${newColor}`;
-
-  // console.log(rgb);
 });
+
+const makeRandomColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
